adb3f33a3555d828266780b81b50e4acb66df32a
Nogueira <anogueirajr@gmail.com>
2021-11-20 21:03:05 -0300

    Varios testes de componentes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,403 @@
+import React from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Heading, Link } from "@chakra-ui/react";
+import { SimpleGrid, Box } from "@chakra-ui/react"
+import { Wrap, WrapItem, Center } from "@chakra-ui/react"
+import { Select } from "@chakra-ui/react"
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react"
+
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from "@chakra-ui/react"
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react"
+
+import {
+  Tag,
+  TagLabel,
+  TagLeftIcon,
+  TagRightIcon,
+  TagCloseButton,
+  Avatar
+} from "@chakra-ui/react"
+
+
+import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react"
+
+import { Progress } from "@chakra-ui/react"
+
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react"
+
+import { useToast } from "@chakra-ui/react"
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react"
+function TransitionExample() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+
+  return (
+    <>
+      <Button onClick={onOpen}>Discard</Button>
+      <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader>Discard Changes?</AlertDialogHeader>
+          {/* <AlertDialogCloseButton /> */}
+          <AlertDialogBody>
+            Are you sure you want to discard all of your notes? 44 words will be
+            deleted.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              No
+            </Button>
+            <Button colorScheme="red" ml={3}>
+              Yes
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  )
+}
+
+function AlertDialogExample() {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const onClose = () => setIsOpen(false)
+  const cancelRef = React.useRef()
+
+  return (
+    <>
+      <Button colorScheme="red" onClick={() => setIsOpen(true)}>
+        Delete Customer
+      </Button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Customer
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  )
+}
+import { useDisclosure } from "@chakra-ui/react"
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react"
+
+import {Input} from "@chakra-ui/react"
+
+function DrawerExample() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+  return (
+    <>
+      <Button ref={btnRef} colorScheme="teal" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuIcon,
+  MenuCommand,
+  MenuDivider,
+} from "@chakra-ui/react"
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react"
+
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  PopoverCloseButton,
+  Portal
+} from "@chakra-ui/react"
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Collapse
+} from "@chakra-ui/react"
+
+function BasicUsage() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* <Lorem count={2} /> */}
+            gsd ra rr re Yesry Are
+            a er eh
+            ae ModalHeader
+            aboute rhaer
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+
+import { PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+
+import { Fade, ScaleFade, Slide, SlideFade } from "@chakra-ui/react"
+
+function FadeEx() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <Fade in={isOpen}>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          Fade
+        </Box>
+      </Fade>
+    </>
+  )
+}
+
+function ScaleFadeEx() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <ScaleFade initialScale={0.9} in={isOpen}>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          Fade
+        </Box>
+      </ScaleFade>
+    </>
+  )
+}
+
+function SlideEx() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          saf gER gweHweTH wrthwH eae rhaer
+          e heqr
+          qe hetheh eheh
+          eaer herqh er
+        </Box>
+      </Slide>
+    </>
+  )
+}
+
+function SlideFadeEx() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <SlideFade in={isOpen} offsetY="20px">
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+adverh ehwtEhweheRqgQrehvqeQehWt
+
+
+vehveh
+wthv
+
+wvth
+
+        </Box>
+      </SlideFade>
+    </>
+  )
+}
+
+
+function CollapseEx() {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onToggle}>Click Me</Button>
+      <Collapse in={isOpen} animateOpacity>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        >
+          wetwetewfweew  we w jwen wn wenf ewiufnwe nfwebf wwie wbe want wn
+           wnwe bgwiubgfwu eonwuebg weuib gwueibg wbwu bgwiubgfwu
+            nwugbwuo gbwue bwge
+
+        </Box>
+      </Collapse>
+    </>
+  )
+}
+
 
 export default function Home() {
   return (
+    
     <div className={styles.container}>
+      {/* <Heading as="h1" size="2xl" mb="2">
+        Welcome to Next.js!
+      </Heading> */}
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="Generated by create next app" />
@@ -12,7 +405,325 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+<CollapseEx/>
+<SlideFadeEx/>
+<SlideEx/>
+<ScaleFadeEx/>
+<FadeEx/>
+
+<PhoneIcon />
+
+<AddIcon w={6} h={6} />
+
+<WarningIcon w={8} h={8} color="red.500" />
+
+
+<Tabs>
+  <TabList>
+    <Tab>One</Tab>
+    <Tab>Two</Tab>
+    <Tab>Three</Tab>
+  </TabList>
+
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>three!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+
+<Tabs variant="enclosed">
+  <TabList>
+    <Tab>One</Tab>
+    <Tab>Two</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+
+
+<Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex="1" textAlign="left">
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex="1" textAlign="left">
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+
+
+<Accordion defaultIndex={[0]} allowMultiple>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex="1" textAlign="left">
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex="1" textAlign="left">
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+
+
+
+<Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton _expanded={{ bg: "tomato", color: "white" }}>
+        <Box flex="1" textAlign="left">
+          Click me to see a different style
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
+
+
+<Popover>
+  <PopoverTrigger>
+    <Button>Trigger</Button>
+  </PopoverTrigger>
+  <PopoverContent>
+    <PopoverArrow />
+    <PopoverCloseButton />
+    <PopoverHeader>Confirmation!</PopoverHeader>
+    <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+  </PopoverContent>
+</Popover>
+
+
+<Popover>
+  <PopoverTrigger>
+    <Button>Trigger</Button>
+  </PopoverTrigger>
+  <Portal>
+    <PopoverContent>
+      <PopoverArrow />
+      <PopoverHeader>Header</PopoverHeader>
+      <PopoverCloseButton />
+      <PopoverBody>
+        <Button colorScheme="blue">Button</Button>
+      </PopoverBody>
+      <PopoverFooter>This is the footer</PopoverFooter>
+    </PopoverContent>
+  </Portal>
+</Popover>
+
+
+<BasicUsage/>
+<Menu>
+  {/* <MenuButton as={Button} rightIcon={<ChevronDownIcon />}> */}
+  <MenuButton as={Button} >
+    Actions
+  </MenuButton>
+  <MenuList>
+    <MenuItem>Download</MenuItem>
+    <MenuItem>Create a Copy</MenuItem>
+    <MenuItem>Mark as Draft</MenuItem>
+    <MenuItem>Delete</MenuItem>
+    <MenuItem>Attend a Workshop</MenuItem>
+  </MenuList>
+</Menu>
+
+<DrawerExample/>
+
+<AlertDialogExample/>
+<TransitionExample/>
+      <Box padding="6" boxShadow="lg" bg="white">
+  <SkeletonCircle size="10" />
+  <SkeletonText mt="4" noOfLines={4} spacing="4" />
+</Box>
+
+
+<Progress value={20} size="xs" colorScheme="pink" />
+
+<Progress size="xs" isIndeterminate />
+
+<CircularProgress value={50} color="green.400">
+  <CircularProgressLabel>50%</CircularProgressLabel>
+</CircularProgress>
+
+<CircularProgress value={30} />
+
+<CircularProgress isIndeterminate color="green.300" />
+
+      <SimpleGrid columns={2} spacing={10}>
+  <Box bg="tomato" width="80px" height="80px">1</Box>
+  <Box bg="tomato" width="80px" height="80px">2</Box>
+  <Box bg="tomato" width="80px" height="80px">3</Box>
+  <Box bg="tomato" width="80px" height="80px">4</Box>
+  <Box bg="tomato" width="80px" height="80px">5</Box>
+</SimpleGrid>
+
+<Wrap>
+  <WrapItem>
+    <Center w="180px" h="80px" bg="red.200">
+      Box 1
+    </Center>
+  </WrapItem>
+  <WrapItem>
+    <Center w="180px" h="80px" bg="green.200">
+      Box 2
+    </Center>
+  </WrapItem>
+  <WrapItem>
+    <Center w="180px" h="80px" bg="tomato">
+      Box 3
+    </Center>
+  </WrapItem>
+  <WrapItem>
+    <Center w="180px" h="80px" bg="blue.200">
+      Box 4
+    </Center>
+  </WrapItem>
+</Wrap>
+<Select placeholder="Select option">
+  <option value="option1">Option 1</option>
+  <option value="option2">Option 2</option>
+  <option value="option3">Option 3</option>
+</Select>        
+
+<Slider aria-label="slider-ex-1" defaultValue={30}>
+  <SliderTrack>
+    <SliderFilledTrack />
+  </SliderTrack>
+  <SliderThumb />
+</Slider>
+
+<Slider defaultValue={60} min={0} max={300} step={30}>
+  <SliderTrack bg="red.100">
+    <Box position="relative" right={10} />
+    <SliderFilledTrack bg="tomato" />
+  </SliderTrack>
+  <SliderThumb boxSize={6} />
+</Slider>
+<Stat>
+  <StatLabel>Collected Fees</StatLabel>
+  <StatNumber>£0.00</StatNumber>
+  <StatHelpText>Feb 12 - Feb 28</StatHelpText>
+</Stat>
+
+<Table variant="simple">
+  <TableCaption>Imperial to metric conversion factors</TableCaption>
+  <Thead>
+    <Tr>
+      <Th>To convert</Th>
+      <Th>into</Th>
+      <Th isNumeric>multiply by</Th>
+    </Tr>
+  </Thead>
+  <Tbody>
+    <Tr>
+      <Td>inches</Td>
+      <Td>millimetres (mm)</Td>
+      <Td isNumeric>25.4</Td>
+    </Tr>
+    <Tr>
+      <Td>feet</Td>
+      <Td>centimetres (cm)</Td>
+      <Td isNumeric>30.48</Td>
+    </Tr>
+    <Tr>
+      <Td>yards</Td>
+      <Td>metres (m)</Td>
+      <Td isNumeric>0.91444</Td>
+    </Tr>
+  </Tbody>
+  <Tfoot>
+    <Tr>
+      <Th>To convert</Th>
+      <Th>into</Th>
+      <Th isNumeric>multiply by</Th>
+    </Tr>
+  </Tfoot>
+</Table>
+
+<Tag size="lg" colorScheme="red" borderRadius="full">
+  <Avatar
+    src="https://bit.ly/sage-adebayo"
+    size="xs"
+    name="Segun Adebayo"
+    ml={-1}
+    mr={2}
+  />
+  <TagLabel>Segun</TagLabel>
+</Tag>
+
+
+
+
+
+<h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
