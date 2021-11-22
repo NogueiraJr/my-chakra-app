23c4d3e5647b480b77c942f55daa40d716990703
Nogueira <anogueirajr@gmail.com>
2021-11-22 12:03:56 -0300

    Mais alguns testes

--- pages/index.js
+++ pages/index.js
@@ -390,6 +390,9 @@ function CollapseEx() {
   )
 }
 
+import { AspectRatio } from "@chakra-ui/react"
+
+import { Flex, Spacer, Square, Text } from "@chakra-ui/react"
 
 export default function Home() {
   return (
@@ -405,6 +408,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+
+<Flex color="white">
+  <Center w="100px" bg="green.500">
+    <Text>Box 1</Text>
+  </Center>
+  <Square bg="blue.500" size="150px">
+    <Text>Box 2</Text>
+  </Square>
+  <Box flex="1" bg="tomato">
+    <Text>Box 3</Text>
+  </Box>
+</Flex>
+
+      <AspectRatio ratio={16 / 9}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.952912260219!2d3.375295414770757!3d6.5276316452784755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2ae68280c1%3A0xdc9e87a367c3d9cb!2sLagos!5e0!3m2!1sen!2sng!4v1567723392506!5m2!1sen!2sng"
+          alt="demo"
+        />
+      </AspectRatio>
+
 <CollapseEx/>
 <SlideFadeEx/>
 <SlideEx/>
